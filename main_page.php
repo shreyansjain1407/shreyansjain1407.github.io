a61c39f96f23a185af75f27889daf4a91d46010e
shreyansjain1407 <42056701+shreyansjain1407@users.noreply.github.com>
2021-09-05 19:57:39 -0400

    .

--- main_page.php
+++ main_page.php
@@ -460,20 +460,3 @@
     </body>
 
 </html>
-<?php
-
-    if(isset($_POST['name'])){
-        $name = $_POST['name'];
-    }
-    if(isset($_POST['email'])){
-        $email = $_POST['email'];
-    }
-    if(isset($_POST['subject'])){
-        $subject = $_POST['subject'];
-    }
-    if(isset($_POST['message'])){
-        $message = $_POST['message'];
-    }
-
-
-?>
